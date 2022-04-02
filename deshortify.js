f68199457248af566272e464764169cd110417a8
Austin Decker <austrideck@gmail.com>
2022-04-02 01:13:06 -0400

    Button to open new non-short tab

---
+++ deshortify.js
@@ -0,0 +1,18 @@
+function openPage(tab) {
+
+	const url = tab.url;
+	const url_re = /.*youtube\.com\/shorts\/(.+)/;
+	const url_match = url_re.exec(url);
+
+	if (url_match == null) {
+		return;
+	}
+
+	const video_id = url_match[1];
+	
+	browser.tabs.create({
+		url: "https://www.youtube.com/watch?v=" + video_id
+	});
+}
+
+browser.browserAction.onClicked.addListener(openPage);
