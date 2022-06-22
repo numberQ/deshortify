fb1b16561e51c9ddb8e082c789c8f7d6f32081ea
Austin Decker <austrideck@gmail.com>
2022-06-22 00:11:13 -0400

    * fixes bug where opening a Shorts URL in a new tab would load the non-Shorts URL in the current tab * removes Shorts URL from browser history when loading non-Shorts URL (does not remove from session history yet)

--- deshortify.js
+++ deshortify.js
@@ -3,11 +3,13 @@ const url_pattern = "*://*.youtube.com/shorts/*";
 const url_filter = {
 	urls: [url_pattern]
 };
-
-function openPage(url) {
 	
-	const url_re = /.*youtube\.com\/shorts\/(.+)/;
-	const url_match = url_re.exec(url);
+const url_regex = /.*youtube\.com\/shorts\/(.+)/;
+
+function openPage(tab) {
+
+	var url = tab.url
+	var url_match = url_regex.exec(url);
 
 	if (url_match == null) {
 		return;
@@ -15,29 +17,32 @@ function openPage(url) {
 
 	console.log("Deshortify: navigating to regular YouTube watch page...");
 
-	const video_id = url_match[1];
+	var video_id = url_match[1];
 	
-	browser.tabs.update({
+	browser.tabs.update(tab.id, {
 		url: "https://www.youtube.com/watch?v=" + video_id
+	});
+
+	browser.history.deleteUrl({
+		url: url
 	});
 }
 
-browser.browserAction.onClicked.addListener(function(tab) {
-	openPage(tab.url);
-});
-
-
 function onGot(item) {
 	browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	    if (item.pageload && changeInfo.status == "complete") {
-	    	openPage(tab.url);
-	    }
+		if (item.pageload && changeInfo.status == "complete") {
+			openPage(tab);
+		}
 	}, url_filter);
 }
 
 function onError(e) {
 	console.log(`Error: ${e}`);
 }
+
+browser.browserAction.onClicked.addListener(function(tab) {
+	openPage(tab);
+});
 
 let getting = browser.storage.sync.get("pageload");
 getting.then(onGot, onError);
